2ce82f864825dd5da79969f946432ef3aa2a668d
piangerellisofyane <piangerellisofyane@gmail.com>
2025-03-10 07:10:33 +0000

    Add 5.java

---
+++ src/5.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class RandomGen {
+    public static void main(String[] args) {
+        int num = (int)(Math.random() * 10);
+        System.out.println("The random number is: " + num);
+    }
+}
