8638452c081f8207a7e7d6f14d55cb944fcacce7
piangerellisofyane <piangerellisofyane@gmail.com>
2025-03-16 05:21:21 +0000

    Add 11.java

---
+++ src/11.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class RandomNumber{
+    public static void main(String args[]){
+        Random rand = new Random();
+        int num = rand.nextInt((10 - 5) + 1) + 5;
+        System.out.println("Your random number is: " +num);
+    }
+}
