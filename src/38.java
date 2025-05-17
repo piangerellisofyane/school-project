0028385f1c038398283c4e4759c531dfb6bf42b6
piangerellisofyane <piangerellisofyane@gmail.com>
2025-05-17 09:07:23 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,24 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example of using a while loop in Java
+
+        int i = 0;
+        do {
+            System.out.println("Looping...");
+            i++;
+        } while (i < 10);
+
+        // Another example of using for-loop
+        for (int j = 0; j <= 5; j++) {
+            System.out.println(j);
+        }
+
+        // Example of using an if-else statement
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("成年人");
+        } else {
+            System.out.println("未成年人");
+        }
+    }
+}
