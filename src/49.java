27425e39d78ec7b42342b144d76fa0c986a57823
piangerellisofyane <piangerellisofyane@gmail.com>
2025-05-27 15:12:52 +0000

    Add 49.java

---
+++ src/49.java
@@ -0,0 +1,13 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Sample code to demonstrate the use of Java classes and methods
+        System.out.println("Hello, World!");
+        
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a child.");
+        }
+    }
+}
